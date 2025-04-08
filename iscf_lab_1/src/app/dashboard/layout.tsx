8d38c6291a6a6f27f611399e867c7b4b6e4b4b06
fgoncalves02 <francisco.rodrigoncalves@gmail.com>
2025-04-08 20:52:33 +0100

    Delete iscf_lab_1/src/app/dashboard directory

--- iscf_lab_1/src/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <div>
-        <header>Meu Header</header>
-        <main>{children}</main>
-        <footer>Meu Footer</footer>
-      </div>
-    );
-  }
